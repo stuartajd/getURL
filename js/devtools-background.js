5d4f87ace6735d01e25f1cb9c27142a5b7aacf98
Dan Brown <ssddanbrown@googlemail.com>
2018-02-25 20:14:13 +0000

    Added panel to show in devtools
    Panel displays query string parameters for currently inspected element
    if it has a href attribute.

---
+++ js/devtools-background.js
@@ -0,0 +1,43 @@
+// Share a reference to our custom sidebar control
+let sidebar = null;
+
+// Create a custom sidebar control
+chrome.devtools.panels.elements.createSidebarPane("Query Params", sidebarInstance => {
+	sidebar = sidebarInstance;
+	sidebar.setObject({});
+});
+
+// Listen to element selection change and get element HREF on change
+chrome.devtools.panels.elements.onSelectionChanged.addListener(_ => {
+	chrome.devtools.inspectedWindow.eval("$0.getAttribute('href')", (result, isException) => {
+  		setSidebarParams(result);
+    });
+});
+
+// Extract parameters from the given URL and show them in the sidebar
+function setSidebarParams(url) {
+	let params = getParamsFromUrl(url);
+	sidebar.setObject(params);
+}
+
+// Get query parameters from a URL as an Object
+function getParamsFromUrl(url) {
+	let params = {};
+	if (!url) return params;
+
+	let searchParams = new URLSearchParams(new URL(url).search);
+	for (let pair of searchParams.entries()) {
+		let key = pair[0];
+		let value = pair[1];
+
+		// Stack values in an array if mulitple for a single key
+		if (typeof params[key] !== 'undefined' && !Array.isArray(params[key])) {
+			params[key] = [params[key]];
+			params[key].push(value);
+			continue;
+		}
+
+		params[key] = value;
+	}
+	return params;
+}
